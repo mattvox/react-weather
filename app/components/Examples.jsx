546275986a803c97cc47f3be08731ba7c660e5f2
mattvox <mattbliffert@icloud.com>
2017-02-13 15:41:27 -0500

    Updated copy.

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,16 +1,11 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-//     render: function () {
-//         return (
-//             <h3>Examples component</h3>
-//         );
-//     }
-// });
-
 var Examples = (props) => {
     return (
-        <h3>Examples component</h3>
+        <div>
+            <h3>Examples</h3>
+            <p>Welcome to the Examples page!</p>
+        </div>
     );
 };
 
