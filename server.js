db5d29703b47f0bd5e36f11f16bb04343ff27a31
mattvox <mattbliffert@icloud.com>
2017-02-13 15:22:16 -0500

    Fixed routes.

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ var app = express();
 //   }
 // });
 
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, function () {
