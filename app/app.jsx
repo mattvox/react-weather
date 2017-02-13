c5934ff7be13f7f2ce7608f30275d98bc6a2791f
mattvox <mattbliffert@icloud.com>
2017-02-13 16:08:15 -0500

    Added Foundation and jQuery.

--- app/app.jsx
+++ app/app.jsx
@@ -8,6 +8,11 @@ var Examples = require('Examples');
 
 var { Route, Router, IndexRoute, hashHistory } = require('react-router');
 
+// Load Foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+
 ReactDOM.render(
     <Router history={hashHistory}>
       <Route path='/' component={Main}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx',
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -7,7 +22,6 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-      apiKey: '/.api-key.js',
       Main: 'app/components/Main.jsx',
       Nav: 'app/components/Nav.jsx',
       Weather: 'app/components/Weather.jsx',
